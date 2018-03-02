76e9400f779a030f5c2cd3bd9c7f27fb1047ae74
monikarym11 <monika.rymsza11@gmail.com>
2018-03-02 15:45:02 +0100

    added conversion and filters

--- ServicesLibrary/Classes/ImageService.cs
+++ ServicesLibrary/Classes/ImageService.cs
@@ -3,6 +3,8 @@ using ImageAcquisitionLibrary.Interfaces;
 using ContractLibrary.Models;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 
 namespace ServicesLibrary.Classes
 {
@@ -14,11 +16,70 @@ namespace ServicesLibrary.Classes
         {
             _imageAcquisition = imageAcquisition;
         }
-        public Task<CameraImageResponse> GetImage()
+        public Bitmap FromBase64Converter(string image)
         {
-            var cameraImageResponse = _imageAcquisition.GetImage();
-            
+            byte[] imageBytes = System.Convert.FromBase64String(image);
+            Bitmap convertedImage;
+            using (MemoryStream ms = new MemoryStream(imageBytes))
+            {
+                convertedImage = new Bitmap(ms);
+            }
+            return convertedImage;
+            // throw new System.NotImplementedException();
+        }
+        public Bitmap BlackAndWhiteImage(Bitmap image)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+
+                    int BWColor;
+                    if ((pixelColor.R + pixelColor.G + pixelColor.B) / 3 > 115)
+                        BWColor = 255;
+                    else
+                        BWColor = 0;
+                    pixelColor = Color.FromArgb(BWColor, BWColor, BWColor);
+
+                    image.SetPixel(x, y, pixelColor);
+
+                }
+            }
+            return image;
+        }
+        public Bitmap GreyscaleImage(Bitmap image)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+                    int greyColor = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    pixelColor = Color.FromArgb(greyColor, greyColor, greyColor);
+                    image.SetPixel(x, y, pixelColor);
+
+                }
+            }
+            return image;
+        }
+        public string ToBase64Converter(byte[] imageBytes)
+        {
+            string base64string = System.Convert.ToBase64String(imageBytes);
+            return base64string;
+        }
+        public async Task<CameraImageResponse> GetImage()
+        {
+            var cameraImageResponse = await _imageAcquisition.GetImage();
+            string base64image = cameraImageResponse.Base64;
+            MemoryStream stream = new MemoryStream();
+            BlackAndWhiteImage(FromBase64Converter(base64image)).Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] imageStreamByteArray = stream.ToArray();
+            //File.WriteAllBytes(@"C:\Users\rymszmon\source\webapi\pikachuTeamXRAY_back_client\RTGClientv1\lastFrame.jpg", imageStreamByteArray);
+            //File.WriteAllText(@"C:\Users\rymszmon\source\webapi\pikachuTeamXRAY_back_client\RTGClientv1\lastFrame.txt", ToBase64Converter(imageStreamByteArray));
+
             return cameraImageResponse;
         }
+
     }
 }
--- ServicesLibrary/Interfaces/IImageService.cs
+++ ServicesLibrary/Interfaces/IImageService.cs
@@ -1,9 +1,13 @@
 ﻿using ContractLibrary.Interfaces;
+using System.Drawing;
 
 namespace ServicesLibrary.Interfaces
 {
     public interface IImageService : ICameraImage
     {
-
+        Bitmap FromBase64Converter(string image);
+        Bitmap BlackAndWhiteImage(Bitmap image);
+        Bitmap GreyscaleImage(Bitmap image);
+        string ToBase64Converter(byte[] imageBytes);
     }
 }
