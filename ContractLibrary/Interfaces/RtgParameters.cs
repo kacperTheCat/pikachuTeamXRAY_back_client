b91ad35fa7380ca844b9acb3f076a6947b505db6
Hudziak Paulina <paulina.hudziak@tieto.com>
2018-03-06 13:50:09 +0100

    add model and interface RtgParameters

--- ContractLibrary/Interfaces/RtgParameters.cs
+++ ContractLibrary/Interfaces/RtgParameters.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContractLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace ContractLibrary.Interfaces
 {
     interface RtgParameters
     {
+        RtgParametersRequest postRtgParameters();
     }
 }
--- ContractLibrary/Models/RtgParametersRequest.cs
+++ ContractLibrary/Models/RtgParametersRequest.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ContractLibrary.Models
 {
-    class RtgParametersRequest
+    public class RtgParametersRequest
     {
+        public int light;
+        public int contrast;
+        public bool blackWhite;
+        public string patientName;
     }
 }
