a481ff385c8fe20af5bebd90f3e9e905567e87b0
monikarym11 <monika.rymsza11@gmail.com>
2018-03-08 10:51:18 +0100

    rtgmachine control

---
+++ RTGClientv1/Controllers/RTGMachineController.cs
@@ -0,0 +1,27 @@
+﻿using ContractLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RTGClientv1.Controllers
+{
+    public class RTGMachineController : ApiController
+    {
+        public void AddMachine(int machineID, string machineAddress)
+        {
+            RTGMachine rtgMachine = new RTGMachine(machineID, machineAddress);
+        }
+        public bool ConnectMachine(RTGMachine rtgMachine)
+        {
+            if(!rtgMachine.MachineBusy)
+            {
+
+            }
+            return rtgMachine.MachineBusy;
+        }
+
+    }
+}
