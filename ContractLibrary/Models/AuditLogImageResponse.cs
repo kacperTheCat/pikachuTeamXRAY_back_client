d0111677928b1e11894a4e2e1ac709ccaffe9a9e
EU\matutluk <lukasz.matuszak@tieto.com>
2018-03-15 11:25:06 +0100

    Added missing class AuditLoImageResponse and cleared log a bit

---
+++ ContractLibrary/Models/AuditLogImageResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractLibrary.Models
+{
+    public class AuditLogImageResponse
+    {
+        public string xRayImageName;
+        public string base64;
+    }
+}
